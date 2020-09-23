0b6a973dc8e8e449ef73ef2e41d0875fa534cb27
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2020-09-23 18:26:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name="Halil";
+            string surname="ÖZTÜRK";
+            int age=25;
+            string str1 = "My name is " + name + " " + surname+" and I'm "+age+" years old.";
+            string str2 = $"My name is {name} {surname} and I'm {age} years old.";
+            string str3 = string.Format("My name is {0} {1} and I'm {2} years old.",name,surname,age);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+        }
+    }
+}
