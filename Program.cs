76afa8f69b67748d3b05e1656ed88f1080e3dfe9
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2020-09-23 18:38:13 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,15 +6,16 @@ namespace consoleapp
     {
         static void Main(string[] args)
         {
-            string name="Halil";
-            string surname="ÖZTÜRK";
-            int age=25;
-            string str1 = "My name is " + name + " " + surname+" and I'm "+age+" years old.";
-            string str2 = $"My name is {name} {surname} and I'm {age} years old.";
-            string str3 = string.Format("My name is {0} {1} and I'm {2} years old.",name,surname,age);
-            Console.WriteLine(str1);
-            Console.WriteLine(str2);
-            Console.WriteLine(str3);
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("Age: ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine( "My name is " + name + " " + surname+" and I'm "+age+" years old." );
+            Console.WriteLine( $"My name is {name} {surname} and I'm {age} years old." );
+            Console.WriteLine( string.Format("My name is {0} {1} and I'm {2} years old.",name,surname,age));
         }
     }
 }
